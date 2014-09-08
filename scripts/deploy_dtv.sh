112c4c0e85d42d10af9eb7c6ab01ad3bf0d4c713
fbessiere <florian.bessiere@insidergroup.de>
2014-09-08 12:03:34 +0200

    fix deploy script

--- scripts/deploy_dtv.sh
+++ scripts/deploy_dtv.sh
@@ -5,3 +5,4 @@ mvn clean install war:war
 #scp target/dtv.war root@10.20.100.93:/opt/tomcat/webapps/dtv.war
 scp target/dtv.war root@10.20.100.93:/tmp/dtv.war
 ssh root@10.20.100.93 cp /tmp/dtv.war /opt/tomcat/webapps/dtv.war
+ssh root@10.20.100.93 service tomcat restart
