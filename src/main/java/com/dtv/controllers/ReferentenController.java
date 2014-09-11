e8b590732afbc892f21c1cf72cf47b2144f82cc2
fbessiere <florian.bessiere@insidergroup.de>
2014-09-11 09:36:05 +0200

    add new controller

---
+++ src/main/java/com/dtv/controllers/ReferentenController.java
@@ -0,0 +1,28 @@
+package com.dtv.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: florianbessiere
+ * Date: 11.09.14
+ * Time: 09:30
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+public class ReferentenController {
+
+    @RequestMapping(value="/referenten", method = RequestMethod.GET)
+    public ModelAndView getOrga(HttpServletRequest request)
+    {
+
+        return new ModelAndView("referenten.ftl");
+    }
+
+}
